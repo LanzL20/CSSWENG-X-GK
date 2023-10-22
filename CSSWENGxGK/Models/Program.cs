86836a022107addbc377eec7e29aca27012e4e30
Dane Chan <114413054+TheBanana03@users.noreply.github.com>
2023-10-22 19:31:17 +0800

    Add files via upload

---
+++ CSSWENGxGK/Models/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ZXing;
+using ZXing.Common;
+using ZXing.Rendering;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing.Windows.Compatibility;
+using System.IO;
+
+class Program
+{
+    static void generateQRCode(string text, string directoryPath)
+    {
+        if (text is null)
+        {
+            Console.WriteLine("Input text is null. Unable to generate QR code.");
+            return;
+        }
+
+        // Combine the directory path and the file name to create the full file path
+        string filePath = Path.Combine(directoryPath, text + ".png");
+
+        // Create the QR code and save it to the specified file path
+        BarcodeWriter barcodeWriter = new BarcodeWriter();
+        barcodeWriter.Format = BarcodeFormat.QR_CODE;
+        barcodeWriter.Renderer = new BitmapRenderer();
+        Bitmap barcodeBitmap = barcodeWriter.Write(text);
+        barcodeBitmap.Save(filePath, ImageFormat.Png);
+
+        // Display the QR code
+        Console.WriteLine("QR Code saved as: " + filePath);
+        Console.WriteLine(text);
+        Console.WriteLine();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
