32e5eb863692d1c7d13ae700ebf383a97aff6b85
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-10-31 13:22:56 +0800

    Add files via upload

---
+++ CSSWENGxGK/Models/UserRoles.cs
@@ -0,0 +1,9 @@
+namespace CSSWENGxGK.Models {
+    public class Admin : IdentityUser {
+        // Add admin-specific properties here
+    }
+
+    public class User : IdentityUser {
+        // Add user-specific properties here
+    }
+}
