b37031330feb6cf0ee6680c58ccab7b563c8a176
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-01 22:52:46 +0800

    Update VolunteerController.cs

--- CSSWENGxGK/Controllers/VolunteerController.cs
+++ CSSWENGxGK/Controllers/VolunteerController.cs
@@ -2,15 +2,17 @@ using CSSWENGxGK.Data;
 using CSSWENGxGK.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace CSSWENGxGK.Controllers
 {
     public class VolunteerController : Controller
     {
         private readonly ApplicationDbContext _db;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<Volunteer> _userManager;
 
-        public VolunteerController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        public VolunteerController(ApplicationDbContext db, UserManager<Volunteer> userManager)
         {
             _db = db;
             _userManager = userManager;
@@ -28,11 +30,10 @@ namespace CSSWENGxGK.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(Volunteer model)
         {
             if (ModelState.IsValid)
             {
-                // Create a Volunteer object based on the RegisterViewModel
                 var volunteer = new Volunteer
                 {
                     CreatedDate = DateTime.Now,
@@ -42,33 +43,22 @@ namespace CSSWENGxGK.Controllers
                     FirstName = model.FirstName,
                     LastName = model.LastName,
                     Email = model.Email,
-                    PhoneNumber = model.MobileNumber,
+                    MobileNumber = model.MobileNumber,
                     // Set other properties here
                 };
 
-                // Add the Volunteer to the database and save changes
+                // Add the Volunteer to the database
                 _db.T_Volunteer.Add(volunteer);
                 _db.SaveChanges();
 
-                // Assign the "User" role to the new user
-                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
-                var result = await _userManager.CreateAsync(user, model.Password);
-
-                if (result.Succeeded)
-                {
-                    await _userManager.AddToRoleAsync(user, "User");
-                }
-                else
-                {
-                    // Handle user creation errors
-                    // ...
-                }
+                // Assign the "Volunteer" role
+                await _userManager.AddToRoleAsync(volunteer, "Volunteer");
 
                 return RedirectToAction("Register");
             }
 
-            // If ModelState is not valid, return the registration view with validation errors
             return View(model);
         }
+
     }
 }
