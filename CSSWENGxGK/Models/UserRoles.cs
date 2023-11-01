69455144ba6cd2ca1ba73c005ad6e09def65d03c
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-01 22:53:19 +0800

    Delete CSSWENGxGK/Models/UserRoles.cs

--- CSSWENGxGK/Models/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-namespace CSSWENGxGK.Models {
-    public class Admin : IdentityUser {
-        // Add admin-specific properties here
-    }
-
-    public class User : IdentityUser {
-        // Add user-specific properties here
-    }
-}
