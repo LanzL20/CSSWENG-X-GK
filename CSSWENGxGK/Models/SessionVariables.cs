11e049e6987b1c348b9c8b068a238e01dd4fcec8
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-05 22:08:29 +0800

    Add files via upload

---
+++ CSSWENGxGK/Models/SessionVariables.cs
@@ -0,0 +1,9 @@
+namespace CSSWENGxGK.Models;
+
+public class SessionVariables
+{
+    public string Event_chosen;
+    public string user_id;
+    public string first_name;
+    public string last_name;
+}
