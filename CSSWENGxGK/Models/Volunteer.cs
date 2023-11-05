705db0ce25e5bd961d4275ce8cf73e14cc235e1a
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-05 21:03:07 +0800

    Update Volunteer.cs

--- CSSWENGxGK/Models/Volunteer.cs
+++ CSSWENGxGK/Models/Volunteer.cs
@@ -1,11 +1,9 @@
 using System;
-using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CSSWENGxGK.Models
 {
-    public class Volunteer : IdentityUser
+    public class Volunteer
     {
         [Key]
         public int VolunteerID { get; set; }
