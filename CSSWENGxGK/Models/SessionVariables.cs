d63b1dd97b776ccfe0174e434bb3d3e02d232fed
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-06 14:00:41 +0800

    Delete CSSWENGxGK/Models/SessionVariables.cs

--- CSSWENGxGK/Models/SessionVariables.cs
+++
@@ -1,9 +0,0 @@
-namespace CSSWENGxGK.Models;
-
-public class SessionVariables
-{
-    public string Event_chosen;
-    public string user_id;
-    public string first_name;
-    public string last_name;
-}
