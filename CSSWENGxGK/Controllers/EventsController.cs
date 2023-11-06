f4fa06a9d4cdf6f895e850134521e61d8871e2e4
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-07 00:09:16 +0800

    Add files via upload

--- CSSWENGxGK/Controllers/EventsController.cs
+++ CSSWENGxGK/Controllers/EventsController.cs
@@ -5,6 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace CSSWENGxGK.Controllers;
 public class EventsController : Controller
 {
+    private readonly ApplicationDbContext _db;
+    string connectionString = "Server=DESKTOP-SERVS0D;Database=cssweng;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    public EventsController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
     public IActionResult AllEvents()
     {
         return View();
@@ -12,12 +20,43 @@ public class EventsController : Controller
 
     public IActionResult OneEvent(string eventId)
     {
+        int selectedEvent = -1;
+
         if (int.TryParse(eventId, out int eventIdValue))
         {
             HttpContext.Session.SetInt32("Selected_event", eventIdValue);
-            int selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
+            selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
         }
-        return View();
+
+        Console.WriteLine(selectedEvent);
+        // Check if the selectedEvent is valid (greater than 0) before querying the database
+        if (selectedEvent > 0)
+        {
+            // Query the database to get event information based on selectedEvent
+            var eventInfo = _db.T_Event.FirstOrDefault(e => e.EventID == selectedEvent);
+
+            if (eventInfo != null)
+            {
+                // Query the organizers for the event
+                var organizers = _db.T_Organizer.Where(o => o.EventID == selectedEvent).ToList();
+
+                // Store the eventInfo and organizers in ViewBag
+                ViewBag.EventInfo = eventInfo;
+                ViewBag.Organizers = organizers;
+
+                return View(); // Pass the ViewModel to the view
+            }
+            else
+            {
+                // Handle the case where the event is not found in the database
+                return View("EventNotFound");
+            }
+        }
+        else
+        {
+            // Handle the case where the selectedEvent is invalid
+            return View("InvalidEvent");
+        }
     }
 
     public IActionResult VolunteerList()
