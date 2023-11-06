8ab30c0b441133f7abbdb54095323c02aa110225
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-07 00:10:43 +0800

    Add files via upload

--- CSSWENGxGK/Models/EventModel.cs
+++ CSSWENGxGK/Models/EventModel.cs
@@ -8,14 +8,12 @@ namespace CSSWENGxGK.Models
         [Key]
         public int EventID { get; set; }
 
-        public int EventImage { get; set; }
-
         [Required(ErrorMessage = "Event Name is required.")]
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Event Name should be between 2 and 100 characters.")]
         public string EventName { get; set; }
 
         [Required]
-        public DateTime EventTime { get; set; }
+        public DateTime EventDate { get; set; }
 
         [Required]
         public string EventLocation { get; set; }
