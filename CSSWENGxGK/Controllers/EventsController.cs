d56b9819c738c492d9b4c7bb66997deb8df8f321
Dane Chan <114413054+TheBanana03@users.noreply.github.com>
2023-11-09 02:47:28 +0800

    updated addevent

--- CSSWENGxGK/Controllers/EventsController.cs
+++ CSSWENGxGK/Controllers/EventsController.cs
@@ -6,169 +6,175 @@ using System.Data.SqlClient;
 namespace CSSWENGxGK.Controllers;
 public class EventsController : Controller
 {
-    private readonly ApplicationDbContext _db;
-    string connectionString = "Server=localhost\\SQLEXPRESS;Database=cssweng;Trusted_Connection=True;TrustServerCertificate=True;";
-        
-    public EventsController(ApplicationDbContext db)
-    {
-        _db = db;
-    }
+	private readonly ApplicationDbContext _db;
+	string connectionString = "Server=localhost\\SQLEXPRESS;Database=cssweng;Trusted_Connection=True;TrustServerCertificate=True;";
 
-    public IActionResult AllEvents(int pageNumber = 1)
-    {
-        pageNumber = HttpContext.Session.GetInt32("Page_number") ?? 1;
-        int pageSize = 9; // Number of events per page
+	public EventsController(ApplicationDbContext db)
+	{
+		_db = db;
+	}
 
-        // Calculate the start and end index for the events to retrieve
-        int startIndex = (pageNumber - 1) * pageSize;
-        int endIndex = pageNumber * pageSize;
+	public IActionResult AllEvents(int pageNumber = 1)
+	{
+		pageNumber = HttpContext.Session.GetInt32("Page_number") ?? 1;
+		int pageSize = 9; // Number of events per page
 
-        // Query the database to retrieve the events in the specified range
-        var events = _db.T_Event
-            .OrderBy(e => e.EventID)
-            .Skip(startIndex)
-            .Take(pageSize)
-            .Select(e => new Event
-            {
-                EventID = e.EventID, // Store the EventID
-                EventName = e.EventName,
-                EventShortDesc = e.EventShortDesc
-            })
-            .ToList();
+		// Calculate the start and end index for the events to retrieve
+		int startIndex = (pageNumber - 1) * pageSize;
+		int endIndex = pageNumber * pageSize;
 
-        // Pass the list of events to the view
-        return View(events);
-    }
+		// Query the database to retrieve the events in the specified range
+		var events = _db.T_Event
+			.OrderBy(e => e.EventID)
+			.Skip(startIndex)
+			.Take(pageSize)
+			.Select(e => new Event
+			{
+				EventID = e.EventID, // Store the EventID
+				EventName = e.EventName,
+				EventShortDesc = e.EventShortDesc
+			})
+			.ToList();
 
-    public IActionResult OneEvent(string eventId)
-    {
-        int selectedEvent = -1;
+		// Pass the list of events to the view
+		return View(events);
+	}
 
-        if (int.TryParse(eventId, out int eventIdValue))
-        {
-            HttpContext.Session.SetInt32("Selected_event", eventIdValue);
-            selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
-        }
+	public IActionResult OneEvent(string eventId)
+	{
+		int selectedEvent = -1;
 
-        Console.WriteLine(selectedEvent);
-        // Check if the selectedEvent is valid (greater than 0) before querying the database
-        if (selectedEvent > 0)
-        {
-            // Query the database to get event information based on selectedEvent
-            var eventInfo = _db.T_Event.FirstOrDefault(e => e.EventID == selectedEvent);
+		if (int.TryParse(eventId, out int eventIdValue))
+		{
+			HttpContext.Session.SetInt32("Selected_event", eventIdValue);
+			selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
+		}
 
-            if (eventInfo != null)
-            {
-                // Query the organizers for the event
-                var organizers = _db.T_Organizer.Where(o => o.EventID == selectedEvent).ToList();
+		Console.WriteLine(selectedEvent);
+		// Check if the selectedEvent is valid (greater than 0) before querying the database
+		if (selectedEvent > 0)
+		{
+			// Query the database to get event information based on selectedEvent
+			var eventInfo = _db.T_Event.FirstOrDefault(e => e.EventID == selectedEvent);
 
-                // Store the eventInfo and organizers in ViewBag
-                ViewBag.EventInfo = eventInfo;
-                ViewBag.Organizers = organizers;
+			if (eventInfo != null)
+			{
+				// Query the organizers for the event
+				var organizers = _db.T_Organizer.Where(o => o.EventID == selectedEvent).ToList();
 
-                return View(); // Pass the ViewModel to the view
-            }
-            else
-            {
-                // Handle the case where the event is not found in the database
-                return View("EventNotFound");
-            }
-        }
-        else
-        {
-            // Handle the case where the selectedEvent is invalid
-            return View("InvalidEvent");
-        }
-    }
+				// Store the eventInfo and organizers in ViewBag
+				ViewBag.EventInfo = eventInfo;
+				ViewBag.Organizers = organizers;
 
-    public IActionResult BackEvent()
-    {
-        // Get the selectedEvent from the session
-        int selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
-        return RedirectToAction("OneEvent", new { eventId = selectedEvent });
-    }
+				return View(); // Pass the ViewModel to the view
+			}
+			else
+			{
+				// Handle the case where the event is not found in the database
+				return View("EventNotFound");
+			}
+		}
+		else
+		{
+			// Handle the case where the selectedEvent is invalid
+			return View("InvalidEvent");
+		}
+	}
 
-    public IActionResult VolunteerList()
-    {
-        int selectedEvent = -1;
+	public IActionResult BackEvent()
+	{
+		// Get the selectedEvent from the session
+		int selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
+		return RedirectToAction("OneEvent", new { eventId = selectedEvent });
+	}
 
-        selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
+	public IActionResult VolunteerList()
+	{
+		int selectedEvent = -1;
 
-        if (selectedEvent > 0)
-        {
-            // Retrieve the event name based on the current event ID (selectedEvent) and store it in ViewBag
-            var eventInfo = _db.T_Event.FirstOrDefault(e => e.EventID == selectedEvent);
+		selectedEvent = HttpContext.Session.GetInt32("Selected_event") ?? -1;
 
-            // Retrieve the VolunteerIDs that joined the selected event
-            var volunteerIds = _db.T_EventsAttended
-                .Where(e => e.EventID == selectedEvent)
-                .Select(e => e.VolunteerID)
-                .ToList();
+		if (selectedEvent > 0)
+		{
+			// Retrieve the event name based on the current event ID (selectedEvent) and store it in ViewBag
+			var eventInfo = _db.T_Event.FirstOrDefault(e => e.EventID == selectedEvent);
 
-            // Retrieve all volunteer information for the volunteer IDs
-            var volunteersInfo = _db.T_Volunteer
-                .Where(volunteer => volunteerIds.Contains(volunteer.VolunteerID))
-                .Select(volunteer => new
-                {
-                    VolunteerID = volunteer.VolunteerID,
-                    FirstName = volunteer.FirstName,
-                    LastName = volunteer.LastName
-                })
-                .ToList();
+			// Retrieve the VolunteerIDs that joined the selected event
+			var volunteerIds = _db.T_EventsAttended
+				.Where(e => e.EventID == selectedEvent)
+				.Select(e => e.VolunteerID)
+				.ToList();
 
-            // Store the volunteersInfo list in ViewBag
-            ViewBag.EventInfo = eventInfo;
-            ViewBag.volunteersInfo = volunteersInfo;
+			// Retrieve all volunteer information for the volunteer IDs
+			var volunteersInfo = _db.T_Volunteer
+				.Where(volunteer => volunteerIds.Contains(volunteer.VolunteerID))
+				.Select(volunteer => new
+				{
+					VolunteerID = volunteer.VolunteerID,
+					FirstName = volunteer.FirstName,
+					LastName = volunteer.LastName
+				})
+				.ToList();
 
-            // You can access ViewBag.volunteersInfo in your view
-            return View();
-        }
-        else
-        {
-            // Handle the case where the selectedEvent is invalid
-            return View("InvalidEvent");
-        }
+			// Store the volunteersInfo list in ViewBag
+			ViewBag.EventInfo = eventInfo;
+			ViewBag.volunteersInfo = volunteersInfo;
 
-
-        return View();
-    }
+			// You can access ViewBag.volunteersInfo in your view
+			return View();
+		}
+		else
+		{
+			// Handle the case where the selectedEvent is invalid
+			return View("InvalidEvent");
+		}
 
 
-    public IActionResult EditOneEvent(){
-        return View();
-    }
+		return View();
+	}
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult addEvent(Event model)
-    {
-        if (ModelState.IsValid)
-        {
+	public IActionResult EditOneEvent()
+	{
+		return View();
+	}
 
-            Guid uniqueId = Guid.NewGuid();
-            byte[] bytes = uniqueId.ToByteArray();
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public IActionResult AddEvent(Event model)
+	{
+		if (ModelState.IsValid)
+		{
 
-            int generatedID = BitConverter.ToInt32(bytes, 0);
-            generatedID = Math.Abs(generatedID);
+			Guid uniqueId = Guid.NewGuid();
+			byte[] bytes = uniqueId.ToByteArray();
 
-            // Define the SQL insert query
-            string query = "SET IDENTITY_INSERT T_Event ON;" +
-                          "INSERT INTO T_Event (EventID, EventImage, EventName, EventDate, EventLocation, EventShortDesc, EventLongDesc, Status) " +
-                          "VALUES (@GeneratedID, @EventImage, @EventName, @EventDate, @EventLocation, @EventShortDesc, @EventLongDesc, @Status);" +
-                          "SET IDENTITY_INSERT T_Event OFF;";
+			int generatedID = BitConverter.ToInt32(bytes, 0);
+			generatedID = Math.Abs(generatedID);
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@GeneratedID", generatedID);
+			// Define the SQL insert query
+			string query = "SET IDENTITY_INSERT T_Event ON;" +
+						  "INSERT INTO T_Event (EventID, EventName, EventDate, EventLocation, EventShortDesc, EventLongDesc, EventStatus) " +
+						  "VALUES (@GeneratedID, @EventName, @EventDate, @EventLocation, @EventShortDesc, @EventLongDesc, @EventStatus);" +
+						  "SET IDENTITY_INSERT T_Event OFF;";
 
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				using (SqlCommand command = new SqlCommand(query, connection))
+				{
+					command.Parameters.AddWithValue("@GeneratedID", generatedID);
+					command.Parameters.AddWithValue("@EventName", model.EventName);
+					command.Parameters.AddWithValue("@EventDate", model.EventDate);
+					command.Parameters.AddWithValue("@EventLocation", model.EventLocation);
+					command.Parameters.AddWithValue("@EventShortDesc", model.EventShortDesc);
+					command.Parameters.AddWithValue("@EventLongDesc", model.EventLongDesc);
+					command.Parameters.AddWithValue("@EventStatus", 0);
 
-        return RedirectToAction("Register");
-    }
+					command.ExecuteNonQuery();
+				}
+			}
+		}
+
+		return RedirectToAction("Register");
+	}
 }
