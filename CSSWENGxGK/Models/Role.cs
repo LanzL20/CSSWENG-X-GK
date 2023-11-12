a8656ae0ed7f3c7617421a6581ea71f7a5bdd2be
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-13 00:06:45 +0800

    Added role class

---
+++ CSSWENGxGK/Models/Role.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace CSSWENGxGK.Models
+{
+    public class Role : IdentityUserRole<int>
+    {
+        // You can add additional properties or methods specific to your application's roles if needed
+    }
+}
