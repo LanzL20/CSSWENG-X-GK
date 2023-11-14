0fe6b57524b96d7de32f4a15608e6c5f27385d4f
Dane Chan <114413054+TheBanana03@users.noreply.github.com>
2023-11-14 10:33:37 +0800

    Made an eventorganizers model

---
+++ CSSWENGxGK/Models/EventOrganizersModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CSSWENGxGK.Models
+{
+    public class EventOrganizers
+    {
+        public Event Event { get; set; }
+        public List<Organizer> Organizers { get; set; }
+    }
+}
