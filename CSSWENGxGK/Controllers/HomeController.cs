60e626a0f113a86594e64a247074397c37443936
Arvin Teri Tan <91467425+EcolaTan@users.noreply.github.com>
2023-11-16 02:16:28 +0800

    Add files via upload

--- CSSWENGxGK/Controllers/HomeController.cs
+++ CSSWENGxGK/Controllers/HomeController.cs
@@ -2,28 +2,53 @@
 using Microsoft.AspNetCore.Mvc;
 using CSSWENGxGK.Models;
 using Microsoft.AspNetCore.Identity;
-namespace CSSWENGxGK.Controllers;
+using Microsoft.AspNetCore.Http;
 
-public class HomeController : Controller
+namespace CSSWENGxGK.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly UserManager<User> _userManager;
-
-    public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+    public class HomeController : Controller
     {
-        _logger = logger;
-        _userManager = userManager;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager;
 
-    public async Task<IActionResult> Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
 
+            CheckAndHandleCookie(); // Call the method in the constructor if needed
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public async Task<IActionResult> Index()
+        {
+            CheckAndHandleCookie(); // Call the method at the beginning of the action
+
+            // Your existing action logic goes here
+            return View();
+        }
+
+        private void CheckAndHandleCookie()
+        {
+            HttpContext context = ControllerContext.HttpContext;
+
+            if (context?.Request?.Cookies.TryGetValue("MyCookie", out string cookieValue) == true &&
+                !string.IsNullOrEmpty(cookieValue) && int.TryParse(cookieValue, out int userId) &&
+                context.Session != null)
+            {
+                // Store the user ID in the session
+                context.Session.SetInt32("User_ID", userId);
+                // You can perform additional initialization based on the cookie value if needed
+            }
+            else
+            {
+                // Handle the case where context or its properties are null, or where the cookie value is not valid
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
